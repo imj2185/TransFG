a3f22863454724e9618feba04ec3ce1065692bfe
TYSSSY <44118618+TYSSSY@users.noreply.github.com>
2022-12-02 09:37:01 -0500

    Add files via upload

--- finetune.py
+++ finetune.py
@@ -6,12 +6,10 @@ import csv
 
 import torch
 from torch.utils.tensorboard import SummaryWriter
-import torch.distributed as dist
 from tqdm import tqdm
 import numpy as np
 
 from models.modeling_early_exit import VisionTransformer, CONFIGS
-from apex.parallel import DistributedDataParallel as DDP
 from utils.data_utils import get_loader
 
 
@@ -38,17 +36,12 @@ def count_parameters(model):
     params = sum(p.numel() for p in model.parameters() if p.requires_grad)
     return params / 1000000
 
-def reduce_mean(tensor, nprocs):
-    rt = tensor.clone()
-    dist.all_reduce(rt, op=dist.ReduceOp.SUM)
-    rt /= nprocs
-    return rt
 
 def simple_accuracy(preds, labels):
     return (preds == labels).mean()
 
 
-def setup(args, early_exit_th):
+def setup(args):
     # Prepare model
     config = CONFIGS[args.model_type]
     config.split = args.split
@@ -65,12 +58,11 @@ def setup(args, early_exit_th):
     elif args.dataset == "INat2017":
         num_classes = 5089
 
-    model = VisionTransformer(config, args.img_size, zero_head=True, num_classes=num_classes,
-                              early_exit_th=early_exit_th)
+    model = VisionTransformer(config, args.img_size, zero_head=True, num_classes=num_classes)
 
     # model.load_from(np.load(args.pretrained_dir))
     if args.pretrained_model is not None:
-        pretrained_model = torch.load(args.pretrained_model)['model']
+        pretrained_model = torch.load(args.pretrained_model, map_location='cuda:0')['model']
         model.load_state_dict(pretrained_model)
     model.to(args.device)
     num_params = count_parameters(model)
@@ -78,8 +70,6 @@ def setup(args, early_exit_th):
 
 
 def valid(args, model, test_loader):
-    if args.local_rank != -1:
-        model = DDP(model, message_size=250000000, gradient_predivide_factor=get_world_size())
     # Validation!
     eval_losses = AverageMeter()
     exit_layers = []
@@ -96,6 +86,7 @@ def valid(args, model, test_loader):
         batch = tuple(t.to(args.device) for t in batch)
         x, y = batch
         with torch.no_grad():
+            y = y.type(torch.LongTensor).cuda()
             logits, exit_layer = model(x)
             eval_loss = loss_fct(logits, y)
             eval_loss = eval_loss.mean()
@@ -121,13 +112,20 @@ def valid(args, model, test_loader):
     all_preds, all_label = all_preds[0], all_label[0]
     accuracy = simple_accuracy(all_preds, all_label)
     accuracy = torch.tensor(accuracy).to(args.device)
-    dist.barrier()
-    val_accuracy = reduce_mean(accuracy, args.nprocs)
+    val_accuracy = accuracy
 
     val_accuracy = val_accuracy.detach().cpu().numpy()
 
     return val_accuracy, sum(exit_layers)/len(exit_layers)
 
+def th_search(th, old_exit_layer, new_exit_layer):
+    accuracy_percent_diff = (old_exit_layer - new_exit_layer)/12
+    if accuracy_percent_diff <= 0.005:
+        th = th * 2
+    elif accuracy_percent_diff > 0.005:
+        th = th * 0.75
+
+    return th
 
 def main():
     parser = argparse.ArgumentParser()
@@ -140,7 +138,7 @@ def main():
                         default="CUB_200_2011",
                         help="Which dataset.")
     parser.add_argument('--data_root', type=str, default='D:/Datasets/CUB_200_2011')
-    parser.add_argument("--eval_batch_size", default=8, type=int,
+    parser.add_argument("--eval_batch_size", default=32, type=int,
                         help="Total batch size for eval.")
     parser.add_argument('--split', type=str, default='non-overlap',
                         help="Split method")
@@ -162,27 +160,52 @@ def main():
 
 
     train_loader, test_loader = get_loader(args)
+    model_names = ['vit_distil_v3_2', 'vit_distil_v3']
+    model_names = ['vit_early_exit_final_10000_6']
+    for model_name in model_names:
+        args.name = model_name
+        args.pretrained_model = os.path.join('D:/Research Projects/Early_Exit/TransFG-baseline/new_output/',
+                                             args.name) + '_checkpoint.bin'
+        args, model = setup(args)
+        accuracy = []
+        exit_layers = []
+        early_exit_threshold = []
+        early_exit_th = 1e-5
+        old_exit_layer = 12
+        #store_point_exit_layer = 12
+        count = 0
+        while True:
+            if count == 100 or (old_exit_layer - 6) / 12 <= 0.005:
+                break
 
-    accuracy = []
-    exit_layers = []
-    early_exit_threshold = []
-    for i in range(101):
-        early_exit_th = i/100000
-        args, model = setup(args, early_exit_th)
-        with torch.no_grad():
-            val_accuracy, exit_layer = valid(args, model, test_loader)
-            print('early_exit_th = ' + str(early_exit_th))
-            print('val_accuracy = ' + str(val_accuracy))
-            print('exit_layer = ' + str(exit_layer))
-            accuracy.append(val_accuracy)
-            exit_layers.append(exit_layer)
-            early_exit_threshold.append(early_exit_th)
+            with torch.no_grad():
+                val_accuracy, exit_layer = valid(args, model, test_loader)
+            new_exit_layer = exit_layer
+            accuracy_percent_diff = (old_exit_layer - new_exit_layer) / 12
 
-    with open(os.path.join("finetune_logs", args.name)+'.csv', 'w', encoding='UTF8', newline='') as f:
-        writer = csv.writer(f)
-        writer.writerow(accuracy)
-        writer.writerow(exit_layers)
-        writer.writerow(early_exit_threshold)
+            if count == 0 or 0.0025 <= accuracy_percent_diff <= 0.0075:
+                print('early_exit_th = ' + str(early_exit_th))
+                print('val_accuracy = ' + str(val_accuracy))
+                print('exit_layer = ' + str(exit_layer))
+                print('saved_data_count = ' + str(count))
+                accuracy.append(val_accuracy)
+                exit_layers.append(exit_layer)
+                early_exit_threshold.append(early_exit_th)
+                store_point_exit_layer = new_exit_layer
+                count += 1
+
+            early_exit_th = th_search(early_exit_th, old_exit_layer, new_exit_layer)
+            print('old_exit_layer = ' + str(old_exit_layer))
+            print('new_exit_layer = ' + str(new_exit_layer))
+            print('next_threshold = ' + str(early_exit_th))
+            old_exit_layer = store_point_exit_layer
+            model.set_early_exit_th(early_exit_th)
+
+        with open(os.path.join("finetune_logs", args.name)+'.csv', 'w', encoding='UTF8', newline='') as f:
+            writer = csv.writer(f)
+            writer.writerow(accuracy)
+            writer.writerow(exit_layers)
+            writer.writerow(early_exit_threshold)
 
 
 if __name__ == "__main__":
