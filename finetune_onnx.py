1d94cb70061c7d6d221a090e28859f93ef0a1a20
imj2185 <dancingnum@gmail.com>
2023-02-20 08:57:54 -0500

    onnx profile files added

---
+++ finetune_onnx.py
@@ -0,0 +1,283 @@
+import argparse
+import logging
+import os
+import random
+import csv
+
+import torch
+from torch.utils.tensorboard import SummaryWriter
+from tqdm import tqdm
+import numpy as np
+import timeit
+
+from models.modeling_early_exit import VisionTransformer, CONFIGS
+#from models.modeling_early_exit_onnx import VisionTransformer, CONFIGS
+from utils.data_utils import get_loader
+
+
+class AverageMeter(object):
+    """Computes and stores the average and current value"""
+
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+
+def count_parameters(model):
+    params = sum(p.numel() for p in model.parameters() if p.requires_grad)
+    return params / 1000000
+
+
+def simple_accuracy(preds, labels):
+    return (preds == labels).mean()
+
+
+def setup(args):
+    # Prepare model
+    config = CONFIGS[args.model_type]
+    config.split = args.split
+    config.slide_step = args.slide_step
+
+    if args.dataset == "CUB_200_2011":
+        num_classes = 200
+    elif args.dataset == "car":
+        num_classes = 196
+    elif args.dataset == "nabirds":
+        num_classes = 555
+    elif args.dataset == "dog":
+        num_classes = 120
+    elif args.dataset == "INat2017":
+        num_classes = 5089
+    elif args.dataset == 'air':
+        num_classes = 100
+
+    model = VisionTransformer(config, args.img_size, zero_head=True, num_classes=num_classes)
+
+    # model.load_from(np.load(args.pretrained_dir))
+    if args.pretrained_model is not None:
+        pretrained_model = torch.load(args.pretrained_model, map_location='cuda:0')['model']
+        model.load_state_dict(pretrained_model)
+    model.to(args.device)
+    num_params = count_parameters(model)
+    return args, model
+
+
+def valid(args, model, test_loader):
+    # Validation!
+    eval_losses = AverageMeter()
+    exit_layers = []
+
+    model.eval()
+    all_preds, all_label = [], []
+    epoch_iterator = tqdm(test_loader,
+                          desc="Validating... (loss=X.X)",
+                          bar_format="{l_bar}{r_bar}",
+                          dynamic_ncols=True,
+                          disable=False)
+    loss_fct = torch.nn.CrossEntropyLoss()
+    for step, batch in enumerate(epoch_iterator):
+        batch = tuple(t.to(args.device) for t in batch)
+        x, y = batch
+        with torch.no_grad():
+            y = y.type(torch.LongTensor).cuda()
+            logits, exit_layer = model(x)
+            eval_loss = loss_fct(logits, y)
+            eval_loss = eval_loss.mean()
+            eval_losses.update(eval_loss.item())
+
+            preds = torch.argmax(logits, dim=-1)
+
+        exit_layers.append(exit_layer)
+
+        if len(all_preds) == 0:
+            all_preds.append(preds.detach().cpu().numpy())
+            all_label.append(y.detach().cpu().numpy())
+        else:
+            all_preds[0] = np.append(
+                all_preds[0], preds.detach().cpu().numpy(), axis=0
+            )
+            all_label[0] = np.append(
+                all_label[0], y.detach().cpu().numpy(), axis=0
+            )
+        epoch_iterator.set_description("Validating... (loss=%2.5f)" % eval_losses.val)
+        epoch_iterator.set_postfix(exit_layer=exit_layer)
+
+    all_preds, all_label = all_preds[0], all_label[0]
+    accuracy = simple_accuracy(all_preds, all_label)
+    accuracy = torch.tensor(accuracy).to(args.device)
+    val_accuracy = accuracy
+
+    val_accuracy = val_accuracy.detach().cpu().numpy()
+
+    return val_accuracy, sum(exit_layers)/len(exit_layers)
+
+def th_search(th, old_exit_layer, new_exit_layer):
+    accuracy_percent_diff = (old_exit_layer - new_exit_layer)/12
+    if accuracy_percent_diff <= 0.005:
+        th = th * 2
+    elif accuracy_percent_diff > 0.005:
+        th = th * 0.75
+
+    return th
+
+def latency(args, model, test_loader):
+    # Validation!
+    eval_losses = AverageMeter()
+
+    model.eval()
+    all_preds, all_label = [], []
+    epoch_iterator = tqdm(test_loader,
+                          desc="Validating... (loss=X.X)",
+                          bar_format="{l_bar}{r_bar}",
+                          dynamic_ncols=True,
+                          disable=args.local_rank not in [-1, 0])
+    loss_fct = torch.nn.CrossEntropyLoss()
+    start_time = timeit.default_timer()
+    for step, batch in enumerate(epoch_iterator):
+        batch = tuple(t.to(args.device) for t in batch)
+        x, y = batch
+        with torch.no_grad():
+            logits = model(x)
+    evalTime = timeit.default_timer() - start_time
+        
+    return evalTime
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--name", required=False, default='vit_distil_v3_2',
+                        help="Name of this run. Used for monitoring.")
+    parser.add_argument("--pretrained_model", type=str,
+                        default='./vit_car_80000_checkpoint.bin',
+                        help="load pretrained model")
+    parser.add_argument("--dataset", choices=["CUB_200_2011", "car", "dog", "nabirds", "INat2017", "air"],
+                        default="car",
+                        help="Which dataset.")
+    parser.add_argument('--data_root', type=str, default='/home/bassett/Documents/archive/datasets/fine_grained_classification')
+    parser.add_argument("--eval_batch_size", default=16, type=int,
+                        help="Total batch size for eval.")
+    parser.add_argument('--split', type=str, default='non-overlap',
+                        help="Split method")
+    parser.add_argument('--slide_step', type=int, default=12,
+                        help="Slide step for overlap split")
+    parser.add_argument("--model_type", choices=["ViT-B_16", "ViT-B_32", "ViT-L_16",
+                                                 "ViT-L_32", "ViT-H_14"],
+                        default="ViT-B_16",
+                        help="Which variant to use.")
+    parser.add_argument("--img_size", default=448, type=int,
+                        help="Resolution size")
+    parser.add_argument("--local_rank", type=int, default=-1,
+                        help="local_rank for distributed training on gpus")
+    parser.add_argument("--train_batch_size", default=16, type=int,
+                        help="Total batch size for training.")
+
+    parser.add_argument("--do_lat_mem_measure", action="store_true", 
+                        help="do evo search")
+    parser.add_argument("--do_onnx", action="store_true", 
+                        help="do onnx conversion")
+    parser.add_argument("--do_onnx_runtime", action="store_true", 
+                        help="do onnx conversion")
+
+    args = parser.parse_args()
+    import torch
+    args.data_root = '{}/{}'.format(args.data_root, args.dataset)
+    args.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
+    train_loader, test_loader = get_loader(args)
+    early_exit_th = 0.000124979
+    args, model = setup(args)
+    if args.do_lat_mem_measure:
+        import torchprofile
+        model.set_early_exit_th(early_exit_th)
+
+        with torch.no_grad():
+            val_accuracy, exit_layer = valid(args, model, test_loader)
+
+        print(val_accuracy, exit_layer)
+
+        evalTime = latency(args, model, test_loader)
+        print('Evaluation done in total {:.3f} secs ({:.3f} sec per example)'.format(evalTime, evalTime / len(test_loader)))
+
+        # size = (16, 3, args.img_size, args.img_size)
+        # dummy_inputs = (
+        #     torch.ones(size, dtype=torch.float).to(args.device)
+        # )
+
+        # model.eval()
+        # #model.set_early_exit_th(early_exit_th)
+        # macs = torchprofile.profile_macs(model, args=dummy_inputs)
+        # print("MAC: ", macs)
+
+
+    if args.do_onnx:
+        model.eval()
+        import torch.onnx
+        size = (8, 3, args.img_size, args.img_size)
+        dummy_input_1 = torch.randn(size, dtype=torch.float).to(args.device)
+        # threshold = [0.002]
+        # dummy_input_2 = torch.Tensor(threshold).to(args.device)
+
+        # val_accuracy, exit_layer = valid(args, model, test_loader, dummy_input_2)
+        # print(val_accuracy, exit_layer)
+
+        torch.onnx.export(model,
+            args=dummy_input_1,
+            f="vitex_pytorch.onnx",
+            export_params=True,
+            input_names=["x"],
+            output_names=["output"],
+            verbose=True)
+
+
+    if args.do_onnx_runtime:
+        import onnxruntime
+
+        ort_session = onnxruntime.InferenceSession("vitex_pytorch.onnx", providers=['CUDAExecutionProvider', 'CPUExecutionProvider'])
+
+        input11 = np.load('onnx_input.npy')
+        input22 = np.array([0.002]).astype(np.float32)
+        model.eval()
+
+        def to_numpy(tensor):
+            return tensor.detach().cpu().numpy() if tensor.requires_grad else tensor.cpu().numpy()
+
+        s = model(torch.from_numpy(input11[:8]).to(args.device), 6)
+        print(s)
+        ort_outs = ort_session.run(None, {"x": input11[:8]})
+
+        print(ort_outs)
+        
+
+
+
+        #torch.onnx.export(model, dummy_input, "vitex_pytorch.onnx", verbose=True)
+
+    # from pytorch_memlab import MemReporter
+    # size = (8, 3, args.img_size, args.img_size)
+    # dummy_inputs = (
+    #     torch.ones(size, dtype=torch.float).to(args.device)
+    # )
+
+    # reporter = MemReporter(model)
+    # output = model(dummy_inputs)
+    # reporter.report()
+
+    # import torchprofile
+    # macs = torchprofile.profile_macs(model, args=dummy_inputs)
+
+    # print('MACs: ', macs)
+
+
+if __name__ == "__main__":
+    main()
---
+++ models/modeling_early_exit_onnx.py
@@ -0,0 +1,197 @@
+import copy
+import logging
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+from scipy import ndimage
+from torch.nn import CrossEntropyLoss
+import models.configs as configs
+
+from .modeling import Part_Attention, Block, LayerNorm, LabelSmoothing, Embeddings, np2th
+
+logger = logging.getLogger(__name__)
+
+def entropy(x):
+    # x: torch.Tensor, logits BEFORE softmax
+    exp_x = torch.exp(x)
+    A = torch.sum(exp_x, dim=1)    # sum of exp(x_i)
+    B = torch.sum(x*exp_x, dim=1)  # sum of x_i * exp(x_i)
+    return torch.log(A) - B/A
+
+
+def con_loss(features, labels):
+    B, _ = features.shape
+    features = F.normalize(features)
+    cos_matrix = features.mm(features.t())
+    pos_label_matrix = torch.stack([labels == labels[i] for i in range(B)]).float()
+    neg_label_matrix = 1 - pos_label_matrix
+    pos_cos_matrix = 1 - cos_matrix
+    neg_cos_matrix = cos_matrix - 0.4
+    neg_cos_matrix[neg_cos_matrix < 0] = 0
+    loss = (pos_cos_matrix * pos_label_matrix).sum() + (neg_cos_matrix * neg_label_matrix).sum()
+    loss /= (B * B)
+    return loss
+
+
+class ViT_early_exit(nn.Module):
+    r"""A module to provide a shortcut
+    from
+    the output of one non-final ViTLayer in ViTEncoder
+    to
+    loss computation
+    """
+    def __init__(self, config, num_classes, zero_heads=True):
+        super(ViT_early_exit, self).__init__()
+        #self.dropout = nn.Dropout(config.transformer['dropout_rate'])
+        self.head = nn.Linear(config.hidden_size, num_classes)
+        self.zero_head = zero_heads
+        with torch.no_grad():
+            if self.zero_head:
+                nn.init.zeros_(self.head.weight)
+                nn.init.zeros_(self.head.bias)
+
+    def forward(self, encoder_output):
+        logits = self.head(encoder_output[:, 0])
+        return logits
+
+
+class Encoder(nn.Module):
+    def __init__(self, config, num_classes, vis, early_exit_th):
+        super(Encoder, self).__init__()
+        self.vis = vis
+        self.layer = nn.ModuleList()
+        self.early_exit_layer = nn.ModuleList()
+        for _ in range(config.transformer["num_layers"]):
+            layer = Block(config, vis)
+            self.layer.append(copy.deepcopy(layer))
+        for _ in range(config.transformer["num_layers"]):
+            early_exit_layer = ViT_early_exit(config, num_classes)
+            self.early_exit_layer.append(copy.deepcopy(early_exit_layer))
+
+        self.encoder_norm = LayerNorm(config.hidden_size, eps=1e-6)
+
+        self.num_layers = config.transformer["num_layers"]
+        self.hidden_size = config.hidden_size
+        self.early_exit_entropy = [-1 for _ in range(config.hidden_size)]
+        self.num_classes = num_classes
+        self.use_lte = True
+        self.early_exit_th = early_exit_th
+        self.init_lte()
+
+    def init_lte(self):
+        self.lte_th = [self.early_exit_th] * self.num_layers
+        #self.lte_classifier = nn.Linear(self.hidden_size, 1)
+        self.lte_classifier = nn.Linear(self.num_classes, 1)
+        self.lte_activation = nn.Sigmoid()
+
+    def forward(self, hidden_states, exit_layer):
+        #new_hidden_states = []
+        #lte_outputs = []
+
+        #choose by single sample
+        for j, layer in enumerate(self.layer):
+            if j < exit_layer:
+                hidden_states, weights = layer(hidden_states)
+                early_exit = self.early_exit_layer[j](self.encoder_norm(hidden_states))
+        
+        return early_exit
+
+
+class Transformer(nn.Module):
+    def __init__(self, config, img_size, num_classes, vis, early_exit_th):
+        super(Transformer, self).__init__()
+        self.embeddings = Embeddings(config, img_size=img_size)
+        self.encoder = Encoder(config, num_classes, vis, early_exit_th)
+
+    def forward(self, input_ids, early_exit):
+        embedding_output = self.embeddings(input_ids)
+        all_early_exit = self.encoder(embedding_output, early_exit)
+        return all_early_exit
+
+
+class VisionTransformer(nn.Module):
+    def __init__(self, config, img_size=224, num_classes=21843, zero_head=False, vis=False, early_exit_th=0.0, train_strategy='continuous-lte'):
+        super(VisionTransformer, self).__init__()
+        self.num_classes = num_classes
+        self.zero_head = zero_head
+        self.classifier = config.classifier
+
+        self.transformer = Transformer(config, img_size, num_classes, vis, early_exit_th)
+        #self.head = nn.Linear(config.hidden_size, num_classes)
+
+        self.num_layers = config.transformer["num_layers"]
+        self.train_strategy = train_strategy
+
+    def set_early_exit_th(self, early_exit_th):
+        self.transformer.encoder.lte_th = [early_exit_th] * self.num_layers
+
+    def forward(self, x, early_exit=9):
+        logits = self.transformer(x, early_exit)
+        return logits
+
+    def load_from(self, weights):
+        with torch.no_grad():
+            # if self.zero_head:
+            #     nn.init.zeros_(self.head.weight)
+            #     nn.init.zeros_(self.head.bias)
+            # else:
+            #     self.head.weight.copy_(np2th(weights["head/kernel"]).t())
+            #     self.head.bias.copy_(np2th(weights["head/bias"]).t())
+
+            self.transformer.embeddings.patch_embeddings.weight.copy_(np2th(weights["embedding/kernel"], conv=True))
+            self.transformer.embeddings.patch_embeddings.bias.copy_(np2th(weights["embedding/bias"]))
+            self.transformer.embeddings.cls_token.copy_(np2th(weights["cls"]))
+            self.transformer.encoder.encoder_norm.weight.copy_(np2th(weights["Transformer/encoder_norm/scale"]))
+            self.transformer.encoder.encoder_norm.bias.copy_(np2th(weights["Transformer/encoder_norm/bias"]))
+
+            posemb = np2th(weights["Transformer/posembed_input/pos_embedding"])
+            posemb_new = self.transformer.embeddings.position_embeddings
+            if posemb.size() == posemb_new.size():
+                self.transformer.embeddings.position_embeddings.copy_(posemb)
+            else:
+                logger.info("load_pretrained: resized variant: %s to %s" % (posemb.size(), posemb_new.size()))
+                ntok_new = posemb_new.size(1)
+
+                if self.classifier == "token":
+                    posemb_tok, posemb_grid = posemb[:, :1], posemb[0, 1:]
+                    ntok_new -= 1
+                else:
+                    posemb_tok, posemb_grid = posemb[:, :0], posemb[0]
+
+                gs_old = int(np.sqrt(len(posemb_grid)))
+                gs_new = int(np.sqrt(ntok_new))
+                print('load_pretrained: grid-size from %s to %s' % (gs_old, gs_new))
+                posemb_grid = posemb_grid.reshape(gs_old, gs_old, -1)
+
+                zoom = (gs_new / gs_old, gs_new / gs_old, 1)
+                posemb_grid = ndimage.zoom(posemb_grid, zoom, order=1)
+                posemb_grid = posemb_grid.reshape(1, gs_new * gs_new, -1)
+                posemb = np.concatenate([posemb_tok, posemb_grid], axis=1)
+                self.transformer.embeddings.position_embeddings.copy_(np2th(posemb))
+
+            for bname, block in self.transformer.encoder.named_children():
+                if bname != 'early_exit_layer':
+                    for uname, unit in block.named_children():
+                        unit.load_from(weights, n_block=uname)
+
+            if self.transformer.embeddings.hybrid:
+                self.transformer.embeddings.hybrid_model.root.conv.weight.copy_(np2th(weights["conv_root/kernel"], conv=True))
+                gn_weight = np2th(weights["gn_root/scale"]).view(-1)
+                gn_bias = np2th(weights["gn_root/bias"]).view(-1)
+                self.transformer.embeddings.hybrid_model.root.gn.weight.copy_(gn_weight)
+                self.transformer.embeddings.hybrid_model.root.gn.bias.copy_(gn_bias)
+
+                for bname, block in self.transformer.embeddings.hybrid_model.body.named_children():
+                    for uname, unit in block.named_children():
+                        unit.load_from(weights, n_block=bname, n_unit=uname)
+
+CONFIGS = {
+    'ViT-B_16': configs.get_b16_config(),
+    'ViT-B_32': configs.get_b32_config(),
+    'ViT-L_16': configs.get_l16_config(),
+    'ViT-L_32': configs.get_l32_config(),
+    'ViT-H_14': configs.get_h14_config(),
+    'testing': configs.get_testing(),
+}
